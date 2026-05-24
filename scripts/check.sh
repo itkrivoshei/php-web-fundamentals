0cdd1ee38e32eb3e2c0def76e0b06f1ea8557dfd
Nikita Krivoshei <69799139+itkrivoshei@users.noreply.github.com>
2026-05-25 00:03:18 +0200

    Make PHP check script portable

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
 set -eu
 
-find . -type f -name '*.php' -print0 | xargs -0 -r -n1 php -l
+find . -type f -name '*.php' -exec sh -c '
+status=0
+for file do
+    php -l "$file" || status=1
+done
+exit "$status"
+' sh {} +
